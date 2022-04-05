f6c589ec604880e51ca309ee7f81d760fcb99d58
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-04-05 19:01:02 +0630

    Header done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 module.exports = {
-	content: ['index.html', './*.{html,js}'],
-	theme: {
-		extend: {},
-		container: {
-			center: true,
-			padding: '2rem',
-		},
-	},
-	plugins: [],
+    mode:'jit',
+    content: [
+        './public/index.html',
+        './src/**/*.{html,js}'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'bookmark-purple': '#5267D4',
+                'bookmark-red': '#FA5959',
+                'bookmark-blue': '#242A45',
+                'bookmark-grey': '#9194A2',
+                'bookmark-white': '#f7f7f7',
+            },
+        },
+        container: {
+            center: true,
+            padding: '2rem',
+        },
+    },
+    plugins: [],
 }
